df3154acb544d85ba91556529514348e3143af09
Javier Enrique Hern�ndez M�rquez <HM210444@alumno.udb.edu.sv>
2021-09-11 20:47:24 -0600

    subiendo trabajo

---
+++ EJER_Teorico/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace EJER_Teorico
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbnum1 = new System.Windows.Forms.TextBox();
+            this.txbnum2 = new System.Windows.Forms.TextBox();
+            this.txbnum3 = new System.Windows.Forms.TextBox();
+            this.txbnum4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(278, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculos Basicos";
+            // 
+            // txbnum1
+            // 
+            this.txbnum1.Location = new System.Drawing.Point(204, 81);
+            this.txbnum1.Name = "txbnum1";
+            this.txbnum1.Size = new System.Drawing.Size(100, 23);
+            this.txbnum1.TabIndex = 1;
+            // 
+            // txbnum2
+            // 
+            this.txbnum2.Location = new System.Drawing.Point(204, 151);
+            this.txbnum2.Name = "txbnum2";
+            this.txbnum2.Size = new System.Drawing.Size(100, 23);
+            this.txbnum2.TabIndex = 2;
+            // 
+            // txbnum3
+            // 
+            this.txbnum3.Location = new System.Drawing.Point(204, 227);
+            this.txbnum3.Name = "txbnum3";
+            this.txbnum3.Size = new System.Drawing.Size(100, 23);
+            this.txbnum3.TabIndex = 3;
+            // 
+            // txbnum4
+            // 
+            this.txbnum4.Location = new System.Drawing.Point(204, 299);
+            this.txbnum4.Name = "txbnum4";
+            this.txbnum4.Size = new System.Drawing.Size(100, 23);
+            this.txbnum4.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(70, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NUMERO 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(70, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "NUMERO 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(70, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "NUMERO 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(70, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "NUMERO 4";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btncalcular.ForeColor = System.Drawing.Color.Purple;
+            this.btncalcular.Location = new System.Drawing.Point(469, 137);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(119, 53);
+            this.btncalcular.TabIndex = 10;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnsalir.ForeColor = System.Drawing.Color.Purple;
+            this.btnsalir.Location = new System.Drawing.Point(469, 269);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(119, 53);
+            this.btnsalir.TabIndex = 11;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbnum4);
+            this.Controls.Add(this.txbnum3);
+            this.Controls.Add(this.txbnum2);
+            this.Controls.Add(this.txbnum1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbnum1;
+        private System.Windows.Forms.TextBox txbnum2;
+        private System.Windows.Forms.TextBox txbnum3;
+        private System.Windows.Forms.TextBox txbnum4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnsalir;
+    }
+}
+
---
+++ EJER_Teorico/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJER_Teorico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //aqui ocupo el boton Calcular para hacer todas las operaciones
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            bool Punto1 = false, Punto2 = false;
+            double num1, num2, num3, num4,sumatoria=0,Mayor,Menor;
+            double[] Numeros = new double[4];
+            num1 = Convert.ToDouble(txbnum1.Text);
+            num2 = Convert.ToDouble(txbnum2.Text);
+            num3 = Convert.ToDouble(txbnum3.Text);
+            num4 = Convert.ToDouble(txbnum4.Text);
+            Numeros[0] = num1;
+            Numeros[1] = num2;
+            Numeros[2] = num3;
+            Numeros[3] = num4;
+
+            //PRIMERA CONDICION
+            //Indicar que los numeros tienen que ser mayores a 0 y no deben repetirse
+            if (num1 > 0 && num2 > 0 && num3 > 0 && num4 > 0)
+            {
+                if (num1 != num2 && num1 != num3 && num1 != num4 && num2 != num3 && num2 != num4 && num3 != num4)
+                {
+                    Punto1 = true;
+                    sumatoria = num1 + num2 + num3 + num4;
+                }
+                else
+                {
+                    MessageBox.Show("Los numeros no pueden repetirse, Digitar correctamente");                  
+                }
+
+            }
+            else            
+                MessageBox.Show("El numero debe ser positivo y mayor que cero, Digitar correctamente");
+
+            //SEGUNDA CONDICION
+            //La sumatoria de los 4 numeros no puede exceder el numero 200 ni ser igual a dicho numero
+            if (Punto1 == true)
+            {
+                if(sumatoria >= 200)
+                {
+                    MessageBox.Show("No se puede seguir porque la sumatoria es igual a 200 o mayor a este numero, porfavor digitar nuevamente los numeros");
+                    txbnum1.Clear();
+                    txbnum2.Clear();
+                    txbnum3.Clear();
+                    txbnum4.Clear();
+
+                }
+                else
+                {
+                    Punto2 = true;
+                }
+            }
+
+            //TERCERA Y CUARTA CONDICION
+            //Aqui aplico la tercera y cuarta condicion del ejercicio, utilice una funcion
+            //para que me diera quien de los 4 numeros era el mayor y el menor
+            //Los numeros digitados en los textbox, Cambian en pantalla segun se cumpla las condiciones
+            if (Punto2 == true)
+            {
+                Mayor = ObtMayorMenor(Numeros, 4);
+                Menor = ObtMayorMenor(Numeros, 1);
+
+                if(Menor > 10)
+                {                  
+                        if (Convert.ToDouble(txbnum1.Text)==Mayor)                    
+                           txbnum1.Text = (Mayor+10).ToString();                  
+                        if (Convert.ToDouble(txbnum2.Text) == Mayor)
+                            txbnum2.Text = (Mayor+10).ToString();
+                       if (Convert.ToDouble(txbnum3.Text) == Mayor)
+                            txbnum3.Text = (Mayor+10).ToString();
+                       if (Convert.ToDouble(txbnum4.Text) == Mayor)
+                            txbnum4.Text = (Mayor+10).ToString();
+                             Mayor = Mayor + 10;
+                    MessageBox.Show($"El mayor numero es {Mayor}, mientras que el menor numero es {Menor}".ToString(), "Calculo de Condicion");
+                }             
+                if (Mayor < 50)
+                {
+                    if (Convert.ToDouble(txbnum1.Text) == Menor)
+                        txbnum1.Text = (Menor - 5).ToString();
+                    if (Convert.ToDouble(txbnum2.Text) == Menor)
+                        txbnum2.Text = (Menor - 5).ToString();
+                    if (Convert.ToDouble(txbnum3.Text) == Menor)
+                        txbnum3.Text = (Menor -5 ).ToString();
+                    if (Convert.ToDouble(txbnum4.Text) == Menor)
+                        txbnum4.Text = (Menor - 5).ToString();
+                    Menor = Menor -5;
+                }
+                MessageBox.Show($"El mayor numero es {Mayor}, mientras que el menor numero es {Menor}".ToString(), "Respuesta Final");
+            }              
+        }
+
+        //Boton para salir del programa
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Funcion que me ayuda a obtener el mayor y menor de los 4 datos digitados
+        //Utilizamos el metodo de la burbuja para hacer el codigo mas corto y ordenado
+        public double ObtMayorMenor(double[] PNumArreglo,int MayorMenor)
+        {
+            double valor, aux;
+            int I=0,j;
+            for (I = 0; I < 3; I++)
+            {
+             for (j = I; j < 3; j++)
+                {
+                    if (PNumArreglo[I] > PNumArreglo[j+1])
+                    {
+                        aux = PNumArreglo[j];
+                        PNumArreglo[j] = PNumArreglo[j+1];
+                        PNumArreglo[j + 1] = aux;
+                    }
+                }
+            }
+            if (MayorMenor == 1)
+            {
+                valor = PNumArreglo[0];
+            }
+            else
+                valor = PNumArreglo[3];
+            return valor;
+        }
+    }
+}
